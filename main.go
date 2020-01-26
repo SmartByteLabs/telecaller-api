caaf379ad0116d9718fecd7591d8607576e617cb
pradip parmar <prince.soamedia@gmail.com>
2020-01-26 23:56:43 +0530

    port read from the config file.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/princeparmar/telecaller-app/controller"
+	"github.com/rightjoin/fig"
 	"github.com/rightjoin/fuel"
 )
 
@@ -11,5 +12,6 @@ func main() {
 	server.AddService(&controller.UploaderService{})
 	server.Version = "1"
 	server.Prefix = "api"
+	server.Port = fig.Int("service.port")
 	server.Run()
 }
